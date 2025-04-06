169c567f40f84556bdd882e06328a31f275b5d08
Adarsh Dhanware <adarshdhanware@gmail.com>
2025-04-07 00:23:44 +0530

    contact page error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,9 @@ import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
 
-    const [state, handleSubmit] = useForm(import.meta.env.VITE_FORMS_PREE_URL);
+    const myForm = import.meta.env.VITE_FORMS_PREE_URL
+
+    const [state, handleSubmit] = useForm(myForm);
     const formRef = useRef();
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
@@ -35,7 +37,7 @@ function Contact() {
                 formRef.current.reset();
             }, 3000);
         }
-    })
+    },[state.succeeded, state.errors])
 
 
 return (
@@ -43,8 +45,13 @@ return (
         <div className='md:px-10 flex flex-col xl:flex-row gap-10 justify-between items-center'>
             <div>
                 <span className='text-6xl uppercase px-22 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text italic'>contact</span>
-                {success && <h1 className="text-md md:text-xl px-22 uppercase">Response sent successfully!</h1>}
-                {error && <h1 className="text-md md:text-xl px-22 uppercase">{error}</h1>}
+                {(success) ? (
+                    <h1 className={`text-md md:text-xl px-22 uppercase ${(success) ? 'visible' : 'hidden'}`}>Response sent successfully!</h1>
+                ) :(
+                    <h1 className={`text-md md:text-xl px-22 uppercase ${(error) ? 'visible' : 'hidden'}`}>{error}</h1>
+                )}
+                {/* {success && <h1 className="text-md md:text-xl px-22 uppercase">Response sent successfully!</h1>}
+                {error && <h1 className="text-md md:text-xl px-22 uppercase">{error}</h1>} */}
             </div>
 
             <div className='w-full md:w-xl'>
